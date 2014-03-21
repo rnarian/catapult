7ab83b88505f7f8ecb58a986e336a8a697efaf8b
Marian Friedmann <marian.friedmann@gmail.com>
2014-03-21 12:00:58 +0100

    grunt conf

---
+++ Gruntfile.js
@@ -0,0 +1,202 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            bower: 'bower_components',
+            css: 'assets/css',
+            js: 'assets/js',
+            images: 'assets/images',
+            icons: 'assets/icons'
+        },
+
+        // SCSS
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    '<%= dirs.css %>/style.css': '<%= dirs.css %>/style.scss'
+                }
+            },
+            build: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    '<%= dirs.css %>/style.css': '<%= dirs.css %>/style.scss'
+                }
+            }
+        },
+
+        // CSS autoprefixer
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            dist: {
+                files: {
+                    '<%= dirs.css %>/style.css': '<%= dirs.css %>/style.css'
+                }
+            }
+        },
+
+        // Connect Server
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: ''
+                }
+            }
+        },
+
+        // Concat
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: [
+                    '<%= dirs.bower %>/jquery/jquery.min.js',
+                    '<%= dirs.js %>/*.js',
+                    '!<%= dirs.js %>/build.js'
+                ],
+                dest: '<%= dirs.js %>/build.js',
+            },
+        },
+
+        // JShint
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                '<%= dirs.js %>/*.js',
+                '!<%= dirs.js %>/build.js'
+            ]
+        },
+
+        // HTMLhint
+        htmlhint: {
+            html: {
+                options: {
+                    'tag-pair': true
+                },
+                src: ['*.html']
+            }
+        },
+
+        // Uglify
+        uglify: {
+            all: {
+                files: {
+                    '<%= dirs.js %>/build.js': ['<%= dirs.js %>/build.js'],
+                    '<%= dirs.js %>/modernizr.js': ['<%= dirs.bower %>/modernizr/modernizr.js'],
+                }
+            }
+        },
+
+        // Imagemin
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.images %>',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: '<%= dirs.images %>'
+                }]
+            }
+        },
+
+
+        // SVGmin
+        svgmin: {
+            options: {
+                plugins: [
+                  { removeViewBox: false },
+                  { removeUselessStrokeAndFill: false }
+                ]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.icons %>',
+                    src: ['*.svg'],
+                    dest: '<%= dirs.icons %>/_min',
+                    ext: '.svg'
+                }]
+            }
+        },
+
+        // Grunticon
+        grunticon: {
+            icons: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.icons %>/_min',
+                    src: ['*.svg', '*.png'],
+                    dest: "<%= dirs.icons %>/grunticon"
+                }],
+                options: {
+                    cssprefix: "icon--"
+                }
+            }
+        },
+
+        // Browser Sync
+        browser_sync: {
+            dev: {
+                bsFiles: {
+                    src : 'assets/css/style.css'
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: ""
+                    },
+                    ghostMode: {
+                        clicks: true,
+                        scroll: true,
+                        links: true,
+                        forms: true
+                    },
+                }
+            }
+        },
+
+        // Watch
+        watch: {
+            options: {
+                livereload: true
+            },
+            sass: {
+                files: ['<%= dirs.css %>/*.scss'],
+                tasks: ['sass:dev', 'autoprefixer']
+            },
+            images: {
+                files: ['<%= dirs.images %>/*.{png,jpg,gif}'],
+                tasks: ['imagemin']
+            },
+            icons: {
+                files: ['<%= dirs.icons %>/*'],
+                tasks: ['svgmin', 'grunticon']
+            },
+            html: {
+                files: ['*.html'],
+                tasks: ['htmlhint']
+            },
+            scripts: {
+                files: ['Gruntfile.js', '<%= dirs.js %>/*.js'],
+                tasks: ['jshint', 'concat'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass:build', 'autoprefixer', 'concat', 'uglify', 'imagemin']);
+    grunt.registerTask('dev', ['connect', 'watch']);
+    grunt.registerTask('dev:sync', ['browser_sync', 'watch']);
+};
