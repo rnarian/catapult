1988376d36236e6445ba55d646ba5335a0fe20e7
Marian Friedmann <marian.friedmann@gmail.com>
2016-01-17 14:53:42 +0100

    Add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ var dirs = {
   images: 'assets/images',
   icons: 'assets/icons'
 }
+gulp.task('sass:dev', function () {
+  gulp.src(dirs.css + '/*.scss')
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(gulp.dest(dirs.css));
+});
 gulp.task('default', function() {
   // place code for your default task here
 });
