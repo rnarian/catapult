6f99992f4fecefb7dc059bded01822011820d616
Marian Friedmann <marian.friedmann@gmail.com>
2016-01-17 14:29:16 +0100

    Add dirs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')();
 
+var dirs = {
+  bower: 'bower_components',
+  css: 'assets/css',
+  js: 'assets/js',
+  images: 'assets/images',
+  icons: 'assets/icons'
+}
 gulp.task('default', function() {
   // place code for your default task here
 });
