668b71b2ce60b0358ba2092ff42873709a1118d7
Marian Friedmann <marian.friedmann@gmail.com>
2016-01-17 14:24:02 +0100

    Add gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
 
 gulp.task('default', function() {
   // place code for your default task here
